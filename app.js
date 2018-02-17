468939ec66dcbd37adf8d2ba38eb419c9dda5d53
hsunyuan <cool8676@gmail.com>
2018-02-18 01:22:46 +0800

    Remove dice image when rolling 1

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
     
 */
 document.querySelector('.dice').style.display = 'none';
-//document.querySelector('#name-0::after').style.content = 'none';
+
 let scores = [0, 0];
 let current = 0;
 let turn = 0;
 let winner = -1;
+let activeClass1 = document.querySelector('.player-0-panel').classList;
+let activeClass2 = document.querySelector('.player-1-panel').classList;
 
 function btn_roll() {
     if(winner !== -1) return;
@@ -19,15 +21,16 @@ function btn_roll() {
 
     if(point === 1) {
         current = 0;
+        
+        document.querySelector('.dice').style.display = 'none';
     } else {
         current += point;  
     }
-    document.querySelector('#current-' + turn).textContent = current;
+    document.getElementById('current-' + turn).textContent = current;
     if(point ===1){
-        console.log(turn);
-        document.querySelector('.player-' + turn +'-panel').classList.remove('active');
+        activeClass1.toggle('active');
+        activeClass2.toggle('active');
         turn = (turn === 0)?1:0;
-        document.querySelector('.player-' + turn +'-panel').classList.add('active');
         
     }
 }
@@ -35,16 +38,17 @@ function btn_roll() {
 function btn_hold() {
     if(current === 0 && winner !== -1) return;
     scores[turn] += current;
-    document.querySelector('#score-' + turn).textContent = scores[turn];
+    document.getElementById('score-' + turn).textContent = scores[turn];
     current = 0;
-    document.querySelector('#current-' + turn).textContent = current;
+    document.getElementById('current-' + turn).textContent = current;
     if(scores[turn] >= 100 && winner === -1) {
         winner = turn;
-        document.querySelector('#name-' + turn).textContent = 'Winner!';
+        document.getElementById('name-' + turn).textContent = 'Winner!';
     }
-    document.querySelector('.player-' + turn +'-panel').classList.remove('active');
+    activeClass1.toggle('active');
+    activeClass2.toggle('active');
     turn = (turn === 0)?1:0;
-    document.querySelector('.player-' + turn +'-panel').classList.add('active');
+    
     
 }
 
@@ -52,8 +56,8 @@ function btn_new() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
     
-    document.querySelector('#name-0').textContent = 'PLAYER 1';
-    document.querySelector('#name-1').textContent = 'PLAYER 2';
+    document.getElementById('name-0').textContent = 'PLAYER 1';
+    document.getElementById('name-1').textContent = 'PLAYER 2';
     winner = -1;
     turn = 0;
     current = 0;
@@ -61,8 +65,8 @@ function btn_new() {
     
     
     for(let i = 0; i < 2; i++){
-    document.querySelector('#current-' + i).textContent = 0;
-    document.querySelector('#score-' + i).textContent = 0;
+    document.getElementById('current-' + i).textContent = 0;
+    document.getElementById('score-' + i).textContent = 0;
     }
     document.querySelector('.dice').style.display = 'none';
 }
